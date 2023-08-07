5317afa96a765e23c845cb10e9c101c521716a9c
Arek Kolodziejczyk <Arek0006@gmail.com>
2023-08-07 18:23:55 +0200

    first commit

---
+++ Source/ArkanoidByWered/ArkanoidByWeredGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArkanoidByWeredGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKANOIDBYWERED_API AArkanoidByWeredGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
