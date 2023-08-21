ba6115c8ec048abae61578b801818fe22f58a33d
Arek Kolodziejczyk <Arek0006@gmail.com>
2023-08-21 15:18:08 +0200

    Created logic for sticking Ball with Paddle, added first implementation for heart animations

--- Source/ArkanoidByWered/ArkanoidByWeredPlayerController.cpp
+++ Source/ArkanoidByWered/ArkanoidByWeredPlayerController.cpp
@@ -2,7 +2,15 @@
 
 
 #include "ArkanoidByWeredPlayerController.h"
+#include "Ball.h"
+#include "HUDWidget.h"
+#include "Paddle.h"
 
+
+AArkanoidByWeredPlayerController::AArkanoidByWeredPlayerController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
 
 void AArkanoidByWeredPlayerController::AddLive()
 {
@@ -14,18 +22,83 @@ void AArkanoidByWeredPlayerController::AddLive()
 
 void AArkanoidByWeredPlayerController::SubLive()
 {
+	UWidgetAnimation* Animation1 = HUD->HeartAnimation1;
+	UWidgetAnimation* Animation2 = HUD->HeartAnimation2;
+	UWidgetAnimation* Animation3 = HUD->HeartAnimation3;
+
+	if (Animation1 && Animation2 && Animation3)
+	{
+		switch (Lives)
+		{
+		case 3:
+			HUD->PlayAnimation(Animation3);
+			break;
+		case 2:
+			HUD->PlayAnimation(Animation2);
+			break;
+		case 1:
+			HUD->PlayAnimation(Animation1);
+			break;
+		default:
+			break;
+		}
+	}
 	if (Lives > 0)
 	{
 		Lives--;
 	}
+	SpawnBall();
 }
 
-int32 AArkanoidByWeredPlayerController::GetLives() const
+void AArkanoidByWeredPlayerController::SpawnBall()
 {
-	return Lives;
+	if (BallClass)
+	{
+		const FVector BallSpawnLocation = FVector(240, 0, -220);
+		const FRotator BallSpawnRotation = FRotator::ZeroRotator;
+
+		SpawnedBall = GetWorld()->SpawnActor<ABall>(BallClass, BallSpawnLocation, BallSpawnRotation);
+	}
 }
 
 void AArkanoidByWeredPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
+
+	SetPaddle();
+
+	if (HUDWidgetClass)
+	{
+		HUD = CreateWidget<UHUDWidget>(GetWorld(), HUDWidgetClass);
+		if (HUD)
+		{
+			HUD->AddToViewport();
+		}
+	}
+	SpawnBall();
+}
+
+void AArkanoidByWeredPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (SpawnedBall && Paddle)
+	{
+		constexpr float Offset{0.01f};
+		FVector NewBallPosition = Paddle->GetActorLocation();
+		NewBallPosition.Z = NewBallPosition.Z + Paddle->GetCollisionHeight() / 2;
+		NewBallPosition.Z = NewBallPosition.Z + SpawnedBall->GetCollisionHeight() / 2;
+		NewBallPosition.Z = NewBallPosition.Z + Offset;
+		
+		SpawnedBall->SetActorLocation(NewBallPosition);
+	}
+}
+
+void AArkanoidByWeredPlayerController::SetPaddle()
+{
+	APawn* ControlledPawn = GetPawn();
+	if (ControlledPawn)
+	{
+		Paddle = Cast<APaddle>(ControlledPawn);
+	}
 }
--- Source/ArkanoidByWered/ArkanoidByWeredPlayerController.h
+++ Source/ArkanoidByWered/ArkanoidByWeredPlayerController.h
@@ -6,23 +6,46 @@
 #include "GameFramework/PlayerController.h"
 #include "ArkanoidByWeredPlayerController.generated.h"
 
-/**
- * 
- */
+class APaddle;
+class ABall;
+class UWidgetAnimation;
+class UHUDWidget;
+
 UCLASS()
 class ARKANOIDBYWERED_API AArkanoidByWeredPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
 public:
+	AArkanoidByWeredPlayerController();
+	
 	void AddLive();
 	void SubLive();
-	int32 GetLives() const;
+
+	void SpawnBall();
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+public:
+	virtual void Tick(float DeltaTime) override;
+
 private:
+	void SetPaddle();
+	
 	int32 Lives{3};
+
+	UPROPERTY(EditDefaultsOnly, Category="UI")
+	TSubclassOf<UHUDWidget> HUDWidgetClass;
+	UPROPERTY()
+	UHUDWidget* HUD;
+
+	UPROPERTY(EditDefaultsOnly, Category="Gameplay")
+	TSubclassOf<ABall> BallClass;
+	UPROPERTY()
+	ABall* SpawnedBall;
+	
+	UPROPERTY()
+	APaddle* Paddle;
 };
