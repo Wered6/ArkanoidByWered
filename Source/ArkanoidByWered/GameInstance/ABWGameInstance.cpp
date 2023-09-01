f8f321f4f033b63b39d014341981e6e3ccea68af
Arek Kolodziejczyk <Arek0006@gmail.com>
2023-09-01 11:27:48 +0200

    Created structure LevelInfo, added instances of 3 levels in GameInstance and added methods to manipulate information about these levels

--- Source/ArkanoidByWered/GameInstance/ABWGameInstance.cpp
+++ Source/ArkanoidByWered/GameInstance/ABWGameInstance.cpp
@@ -3,6 +3,28 @@
 
 #include "ABWGameInstance.h"
 
+UABWGameInstance::UABWGameInstance()
+{
+	FLevelInfo Level1Info;
+	Level1Info.LevelName = TEXT("Level1");
+	Level1Info.bIsLevelUnlocked = true;
+	Level1Info.bIsLevelCompleted = false;
+
+	FLevelInfo Level2Info;
+	Level2Info.LevelName = TEXT("Level2");
+	Level2Info.bIsLevelUnlocked = false;
+	Level2Info.bIsLevelCompleted = false;
+
+	FLevelInfo Level3Info;
+	Level3Info.LevelName = TEXT("Level3");
+	Level3Info.bIsLevelUnlocked = false;
+	Level3Info.bIsLevelCompleted = false;
+
+	LevelsInfo.Add(Level1Info);
+	LevelsInfo.Add(Level2Info);
+	LevelsInfo.Add(Level3Info);
+}
+
 bool UABWGameInstance::GetHasPlayerLost() const
 {
 	return bHasPlayerLost;
@@ -10,5 +32,30 @@ bool UABWGameInstance::GetHasPlayerLost() const
 
 void UABWGameInstance::SetHasPlayerLost(const bool bPlayerHasLost)
 {
-	this->bHasPlayerLost = bPlayerHasLost;
+	bHasPlayerLost = bPlayerHasLost;
+}
+
+FString UABWGameInstance::GetLevelNameByIndex(const int32 LevelIndex) const
+{
+	return LevelsInfo[LevelIndex].LevelName;
+}
+
+bool UABWGameInstance::GetIsLevelUnlockedByIndex(const int32 LevelIndex) const
+{
+	return LevelsInfo[LevelIndex].bIsLevelUnlocked;
+}
+
+bool UABWGameInstance::GetIsLevelCompletedByIndex(const int32 LevelIndex) const
+{
+	return LevelsInfo[LevelIndex].bIsLevelCompleted;
+}
+
+void UABWGameInstance::UnlockLevelByIndex(const int32 LevelIndex)
+{
+	LevelsInfo[LevelIndex].bIsLevelUnlocked = true;
+}
+
+void UABWGameInstance::CompleteLevelByIndex(const int32 LevelIndex)
+{
+	LevelsInfo[LevelIndex].bIsLevelCompleted = true;
 }
--- Source/ArkanoidByWered/GameInstance/ABWGameInstance.h
+++ Source/ArkanoidByWered/GameInstance/ABWGameInstance.h
@@ -6,15 +6,41 @@
 #include "Engine/GameInstance.h"
 #include "ABWGameInstance.generated.h"
 
+USTRUCT()
+struct FLevelInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FString LevelName;
+
+	UPROPERTY()
+	bool bIsLevelUnlocked;
+
+	UPROPERTY()
+	bool bIsLevelCompleted;
+};
+
 UCLASS()
 class ARKANOIDBYWERED_API UABWGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 
 public:
+	UABWGameInstance();
+
 	bool GetHasPlayerLost() const;
-	void SetHasPlayerLost(const bool bHasPlayerLost);
-	
+	void SetHasPlayerLost(const bool bPlayerHasLost);
+
+	FString GetLevelNameByIndex(const int32 LevelIndex) const;
+	bool GetIsLevelUnlockedByIndex(const int32 LevelIndex) const;
+	bool GetIsLevelCompletedByIndex(const int32 LevelIndex) const;
+
+	void UnlockLevelByIndex(const int32 LevelIndex);
+	void CompleteLevelByIndex(const int32 LevelIndex);
+
 private:
 	bool bHasPlayerLost{false};
+
+	TArray<FLevelInfo> LevelsInfo;
 };
