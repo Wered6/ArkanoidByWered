02abd98d00e4addd42b99ce7a7d14f2af2953611
Arek Kolodziejczyk <Arek0006@gmail.com>
2023-09-10 21:17:03 +0200

    Added few functions and variables to system

--- Source/ArkanoidByWered/Systems/BallsAndPaddleSystem/ABWBallsAndPaddleSubsystem.cpp
+++ Source/ArkanoidByWered/Systems/BallsAndPaddleSystem/ABWBallsAndPaddleSubsystem.cpp
@@ -2,4 +2,37 @@
 
 
 #include "ABWBallsAndPaddleSubsystem.h"
+#include "ArkanoidByWered/GameplayElements/ABWBall.h"
+#include "Kismet/GameplayStatics.h"
 
+void UABWBallsAndPaddleSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	check(!bInitialized);
+	bInitialized = true;
+}
+
+void UABWBallsAndPaddleSubsystem::Deinitialize()
+{
+	check(bInitialized);
+	bInitialized = false;
+}
+
+void UABWBallsAndPaddleSubsystem::AddBall(AABWBall* Ball)
+{
+	if (!Ball)
+	{
+		UE_LOG(LogActor, Warning, TEXT("UABWBallsAndPaddleSubsystem::AddBall|Ball is null"));
+		return;
+	}
+	Balls.Add(Ball);
+}
+
+void UABWBallsAndPaddleSubsystem::RemoveBall(AABWBall* Ball)
+{
+	if (!Ball)
+	{
+		UE_LOG(LogActor, Warning, TEXT("UABWBallsAndPaddleSubsystem::RemoveBall|Ball is null"));
+		return;
+	}
+	Balls.Remove(Ball);
+}
