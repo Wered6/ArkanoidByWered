0a08354780b9640819320ad9c072ca71cd4df5b5
Arek Kolodziejczyk <Arek0006@gmail.com>
2023-09-18 23:37:10 +0200

    Split LoadLevelDataFromJson into smaller functions and add more UE_LOGs

--- Source/ArkanoidByWered/Utilities/JSON/JsonUtility.cpp
+++ Source/ArkanoidByWered/Utilities/JSON/JsonUtility.cpp
@@ -4,33 +4,84 @@
 #include "JsonUtility.h"
 #include "ArkanoidByWered/Systems/LevelSystem/ABWLevelData.h"
 
-TArray<UABWLevelData*> FJsonUtility::LoadLevelDataFromJson(UObject* OuterObject, const FString& FilePath)
+bool FJsonUtility::LoadLevelDataFromJson(UObject* OuterObject, const FString& FilePath,
+                                         TArray<UABWLevelData*>& OutLevelsDataArray)
+{
+	const TSharedPtr<FJsonObject> JsonObject = ReadJsonFile(FilePath);
+
+	if (!JsonObject.IsValid())
+	{
+		UE_LOG(LogJson, Error, TEXT("FJsonUtility::LoadLevelDataFromJson|Failed to deserialize JSON from file: %s"),
+		       *FilePath);
+		return false;
+	}
+	if (!ValidateJsonObject(JsonObject))
+	{
+		UE_LOG(LogJson, Error,
+		       TEXT("FJsonUtility::LoadLevelDataFromJson|Malformed JSON: missing or incorrect types for fields."));
+		return false;
+	}
+
+	return PopulateLevelDataArray(OuterObject, JsonObject, OutLevelsDataArray);
+}
+
+TSharedPtr<FJsonObject> FJsonUtility::ReadJsonFile(const FString& FilePath)
 {
 	FString JsonString;
 	FFileHelper::LoadFileToString(JsonString, *FilePath);
-
 	TSharedPtr<FJsonObject> JsonObject;
 	const TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(JsonString);
 
-	TArray<UABWLevelData*> LevelsDataArray;
-
-	if (FJsonSerializer::Deserialize(JsonReader, JsonObject) && JsonObject.IsValid())
+	if (FJsonSerializer::Deserialize(JsonReader, JsonObject))
 	{
-		TArray<TSharedPtr<FJsonValue>> JsonLevels = JsonObject->GetArrayField(TEXT("Levels"));
-
-		for (int i = 0; i < JsonLevels.Num(); i++)
-		{
-			const TSharedPtr<FJsonObject> JsonLevel = JsonLevels[i]->AsObject();
-			const FName LevelName = FName(*JsonLevel->GetStringField(TEXT("LevelName")));
-			const bool bIsLevelUnlocked = JsonLevel->GetBoolField(TEXT("bIsLevelUnlocked"));
-			const bool bIsLevelCompleted = JsonLevel->GetBoolField(TEXT("bIsLevelCompleted"));
-
-			UABWLevelData* NewLevelData = UABWLevelData::CreateUABWLevelData(
-				OuterObject, LevelName, bIsLevelUnlocked, bIsLevelCompleted);
-
-			LevelsDataArray.Add(NewLevelData);
-		}
+		return JsonObject;
 	}
 
-	return LevelsDataArray;
+	return nullptr;
+}
+
+bool FJsonUtility::ValidateJsonObject(const TSharedPtr<FJsonObject>& JsonObject)
+{
+	TArray<TSharedPtr<FJsonValue>> JsonLevels = JsonObject->GetArrayField(TEXT("Levels"));
+
+	for (const auto& JsonLevelPtr : JsonLevels)
+	{
+		const TSharedPtr<FJsonObject> JsonLevel = JsonLevelPtr->AsObject();
+
+		if (!JsonLevel->HasTypedField<EJson::String>(FieldLevelName) ||
+			!JsonLevel->HasTypedField<EJson::Boolean>(FieldIsLevelUnlocked) ||
+			!JsonLevel->HasTypedField<EJson::Boolean>(FieldIsLevelCompleted))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool FJsonUtility::PopulateLevelDataArray(UObject* OuterObject,
+                                          const TSharedPtr<FJsonObject>& JsonObject,
+                                          TArray<UABWLevelData*>& OutLevelsDataArray)
+{
+	TArray<TSharedPtr<FJsonValue>> JsonLevels = JsonObject->GetArrayField(TEXT("Levels"));
+
+	for (const auto& JsonLevelPtr : JsonLevels)
+	{
+		const TSharedPtr<FJsonObject> JsonLevel = JsonLevelPtr->AsObject();
+
+		const FName LevelName = FName(*JsonLevel->GetStringField(FieldLevelName));
+		const bool bIsLevelUnlocked = JsonLevel->GetBoolField(FieldIsLevelUnlocked);
+		const bool bIsLevelCompleted = JsonLevel->GetBoolField(FieldIsLevelCompleted);
+
+		UABWLevelData* NewLevelData = UABWLevelData::CreateUABWLevelData(
+			OuterObject, LevelName, bIsLevelUnlocked, bIsLevelCompleted);
+
+		if (!NewLevelData)
+		{
+			UE_LOG(LogJson, Error, TEXT("FJsonUtility::PopulateLevelDataArray|NewLevelData is nullptr"));
+			return false;
+		}
+
+		OutLevelsDataArray.Add(NewLevelData);
+	}
+	return true;
 }
--- Source/ArkanoidByWered/Utilities/JSON/JsonUtility.h
+++ Source/ArkanoidByWered/Utilities/JSON/JsonUtility.h
@@ -7,5 +7,18 @@ class UABWLevelData;
 class ARKANOIDBYWERED_API FJsonUtility
 {
 public:
-	static TArray<UABWLevelData*> LoadLevelDataFromJson(UObject* OuterObject, const FString& FilePath);
+	static bool LoadLevelDataFromJson(UObject* OuterObject,
+	                                  const FString& FilePath,
+	                                  TArray<UABWLevelData*>& OutLevelsDataArray);
+
+private:
+	static TSharedPtr<FJsonObject> ReadJsonFile(const FString& FilePath);
+	static bool ValidateJsonObject(const TSharedPtr<FJsonObject>& JsonObject);
+	static bool PopulateLevelDataArray(UObject* OuterObject,
+	                                   const TSharedPtr<FJsonObject>& JsonObject,
+	                                   TArray<UABWLevelData*>& OutLevelsDataArray);
+
+	inline static FString FieldLevelName{TEXT("LevelName")};
+	inline static FString FieldIsLevelUnlocked{TEXT("bIsLevelUnlocked")};
+	inline static FString FieldIsLevelCompleted{TEXT("bIsLevelCompleted")};
 };
