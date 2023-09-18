f407e1569a806f2401d5090af6d0850048bec164
Arek Kolodziejczyk <Arek0006@gmail.com>
2023-09-18 21:43:53 +0200

    Added JsonUtility class to seperate logic for loading levels data from json

---
+++ Source/ArkanoidByWered/Utilities/JSON/JsonUtility.cpp
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2023 Wered. All rights reserved.
+
+
+#include "JsonUtility.h"
+#include "ArkanoidByWered/Systems/LevelSystem/ABWLevelData.h"
+
+TArray<UABWLevelData*> FJsonUtility::LoadLevelDataFromJson(UObject* OuterObject, const FString& FilePath)
+{
+	FString JsonString;
+	FFileHelper::LoadFileToString(JsonString, *FilePath);
+
+	TSharedPtr<FJsonObject> JsonObject;
+	const TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(JsonString);
+
+	TArray<UABWLevelData*> LevelsDataArray;
+
+	if (FJsonSerializer::Deserialize(JsonReader, JsonObject) && JsonObject.IsValid())
+	{
+		TArray<TSharedPtr<FJsonValue>> JsonLevels = JsonObject->GetArrayField(TEXT("Levels"));
+
+		for (int i = 0; i < JsonLevels.Num(); i++)
+		{
+			const TSharedPtr<FJsonObject> JsonLevel = JsonLevels[i]->AsObject();
+			const FName LevelName = FName(*JsonLevel->GetStringField(TEXT("LevelName")));
+			const bool bIsLevelUnlocked = JsonLevel->GetBoolField(TEXT("bIsLevelUnlocked"));
+			const bool bIsLevelCompleted = JsonLevel->GetBoolField(TEXT("bIsLevelCompleted"));
+
+			UABWLevelData* NewLevelData = UABWLevelData::CreateUABWLevelData(
+				OuterObject, LevelName, bIsLevelUnlocked, bIsLevelCompleted);
+
+			LevelsDataArray.Add(NewLevelData);
+		}
+	}
+
+	return LevelsDataArray;
+}
---
+++ Source/ArkanoidByWered/Utilities/JSON/JsonUtility.h
@@ -0,0 +1,11 @@
+﻿// Copyright (c) 2023 Wered. All rights reserved.
+
+#pragma once
+
+class UABWLevelData;
+
+class ARKANOIDBYWERED_API FJsonUtility
+{
+public:
+	static TArray<UABWLevelData*> LoadLevelDataFromJson(UObject* OuterObject, const FString& FilePath);
+};
