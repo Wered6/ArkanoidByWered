191be88f165debef006c55686017c6d6e86b2058
Arek Kolodziejczyk <Arek0006@gmail.com>
2023-09-18 21:08:36 +0200

    Added 'Json' and 'JsonUtilities' to PublicDependencyModuleNames in .Build.cs

--- Source/ArkanoidByWered/ArkanoidByWered.Build.cs
+++ Source/ArkanoidByWered/ArkanoidByWered.Build.cs
@@ -7,14 +7,15 @@ public class ArkanoidByWered : ModuleRules
 	public ArkanoidByWered(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[]
+			{ "Core", "CoreUObject", "Engine", "InputCore", "Paper2D", "Json", "JsonUtilities" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
